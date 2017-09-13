8487279c953eaeddc6d0efe81d1ce7908ee9161d
blackmore <blackmore>
2017-09-13 21:55:53 +0100

    Started to add signals and slots, working on PARTS

--- PyTester9001.py
+++ PyTester9001.py
@@ -1,18 +1,57 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from typing import List
+from PyQt5.QtWidgets import QMainWindow, QApplication, QComboBox
 from UI.MainForm import Ui_PyTester9001
+from SQLiteModel import SQLiteModel
 
 
 class PyTester9001(QMainWindow, Ui_PyTester9001):
+    combo_holding_string = "....Please Select"
+
     def __init__(self):
         super(PyTester9001, self).__init__()
         self.setupUi(self)
 
-        # Connect up signals
-        self.startQuizButton.clicked.connect(self.update_text)
+        # Connect to Database
+        self.model = SQLiteModel("SelfTestOTron.sqlite")
+        self.update_ComboBox("moduleComboBox", self.model.modulelist())
 
-    def update_text(self):
-        self.questionText.setText("Test")
+        # Connect signals to slots
+        self.selectDatabaseButton.clicked.connect(self.holding_slot)  # Select Database
+        self.startQuizButton.clicked.connect(self.holding_slot)  # Start Quiz
+        self.moduleComboBox.currentIndexChanged.connect(
+            lambda: self.moduleComboBox_index_changed(self.moduleComboBox))  # On Module Select
+        self.partComboBox.currentIndexChanged.connect(self.holding_slot)  # On Part Select
+        self.chapterCombBox.currentIndexChanged.connect(self.holding_slot)  # On Chapter Select
+        self.revealAnswerButton.clicked.connect(self.holding_slot)  # Reveal Answer
+        self.answerCorrectButton.clicked.connect(self.holding_slot)  # Answer Correct
+        self.answerIncorrectButton.clicked.connect(self.holding_slot)  # Answer Incorrect
+        self.stopQuizButton.clicked.connect(self.holding_slot)  # Stop Quiz
+
+    def update_ComboBox(self, combo_box_name: str, modules: List[str]):
+        if combo_box_name == "moduleComboBox":
+            self.partComboBox.clear()
+            self.chapterCombBox.clear()
+        elif combo_box_name == "partComboBox":
+            self.chapterCombBox.clear()
+
+        combo_box = getattr(self, combo_box_name) # type: QComboBox
+        combo_box.clear()
+
+        combo_box.addItem(self.combo_holding_string)
+        for x in modules:
+            combo_box.addItem(x.capitalize())
+
+
+    # Slots
+    def holding_slot(self):
+        print("holding")
+
+
+    def moduleComboBox_index_changed(self, i):
+        if i != 0:
+            self.update_ComboBox("partComboBox", self.model.partlist(self.moduleComboBox.currentText()))
+
 
 
 if __name__ == '__main__':
--- SQLiteModel.py
+++ SQLiteModel.py
@@ -2,14 +2,11 @@ import sqlite3
 
 
 class SQLiteModel():
-
-    def __init__(self,filepath):
+    def __init__(self, filepath):
         self.conn = self.connect(filepath)
 
     def connect(self, filepath):
-
         return sqlite3.connect(filepath)
-
 
     def modulelist(self):
         curr = self.conn.cursor()
@@ -18,4 +15,9 @@ class SQLiteModel():
         curr.close()
         return modules
 
-
+    def partlist(self, mod: str):
+        curr = self.conn.cursor()
+        curr.execute("SELECT DISTINCT PART FROM QA WHERE Module = '{0}'".format(mod))
+        parts = [str(x[0]) for x in curr.fetchall()]
+        curr.close()
+        return parts
